2d995bf7c5c49c7f044506c2fe0de01abdd82703
sampad_dash <sdash@acurian.com>
2017-10-26 12:01:16 +0530

    Uma urls changed for case sensitive deployments

--- client.example.com/Uma.php
+++ client.example.com/Uma.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <title>uma method test</title>
 <body>
-    <a href="protect.php">uma_rs_protect</a>
+    <a href="Protect.php">uma_rs_protect</a>
     <br>
-    <a href="check_access.php">uma_rs_check_access</a>
+    <a href="Check_access.php">uma_rs_check_access</a>
     <br>
-    <a href="get_rpt.php">uma_rp_get_rpt</a>
+    <a href="Get_rpt.php">uma_rp_get_rpt</a>
     <br>
-    <a href="claims_gathering_url.php">uma_rp_claims_gathering_url</a>
+    <a href="Claims_gathering_url.php">uma_rp_claims_gathering_url</a>
 
 
 <p></p>
